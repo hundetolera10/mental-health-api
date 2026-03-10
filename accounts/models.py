6ea1977b4dd99c5e992335c3072130e0907e3622
Hunde Tolera <hundetolera225@gmail>
2026-03-10 21:08:59 +0300

    Add custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    AGE_GROUP_CHOICES = [
+        ("13-17", "Teen"),
+        ("18-24", "Young Adult"),
+        ("25+", "Adult"),
+    ]
+    age_group = models.CharField(
+        max_length=10, 
+        choices=AGE_GROUP_CHOICES,
+        blank=True,
+        null=True
+    )
+    GENDER_CHOICES = [
+        ("male", "Male"),
+        ("female", "Female"),
+        ("other", "Other"),
+    ]
+    gender = models.CharField(
+        max_length=10,
+        choices=GENDER_CHOICES,
+        blank=True,
+        null=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.username
