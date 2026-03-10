2f2b6430b7e199ff7583ab3376f7da1a7dfe0c4c
Hunde Tolera <hundetolera225@gmail>
2026-03-10 21:17:09 +0300

    Improve custom user model with email login

--- accounts/models.py
+++ accounts/models.py
@@ -28,5 +28,12 @@ class User(AbstractUser):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    class Meta:
+        ordering = ['-created_at']
+
     def __str__(self):
         return self.username
